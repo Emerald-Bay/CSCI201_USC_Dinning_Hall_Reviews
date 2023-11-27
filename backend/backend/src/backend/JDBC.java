29314d7c15a1b959b6ab3db79ba64420c49db4e0
Jeannie Jiang <jeanniej@usc.edu>
2023-11-26 16:16:37 -0800

    Update JDBC.java

--- backend/backend/src/backend/JDBC.java
+++ backend/backend/src/backend/JDBC.java
@@ -63,7 +63,7 @@ public class JDBC {
 	    }
 	}
 	
-	
+	/**
 	//testing for registering new user
 	public static void main(String[] args) {
         JDBC jdbcConnector = new JDBC();
@@ -75,7 +75,7 @@ public class JDBC {
             e.printStackTrace();
         }
     }
-
+	**/
 	
 	public boolean returningUser(String username, String password) throws ClassNotFoundException{
         Connection connection = null;
@@ -139,10 +139,10 @@ public class JDBC {
   	**/
 	
 	//reviews JDBC: username, password, title, body, rating
-	public int insertReview(String username, String password, String title, String body, double rating) {
+	public int insertReview(String diningHall, String username, String password, String title, String body, double rating) {
 	    try (Connection connection = getConnection();
 	         PreparedStatement checkUserStatement = connection.prepareStatement("SELECT * FROM Users WHERE username = ? AND password = ?");
-	         PreparedStatement insertReviewStatement = connection.prepareStatement("INSERT INTO Reviews (username, password, title, body, rating) VALUES (?, ?, ?, ?, ?)")) {
+	         PreparedStatement insertReviewStatement = connection.prepareStatement("INSERT INTO Reviews (diningHall, username, password, title, body, rating) VALUES (?, ?, ?, ?, ?, ?)")) {
 
 	        checkUserStatement.setString(1, username);
 	        checkUserStatement.setString(2, password);
@@ -153,11 +153,12 @@ public class JDBC {
 	            return -1; 
 	        }
 
-	        insertReviewStatement.setString(1, username);
-	        insertReviewStatement.setString(2, password);
-	        insertReviewStatement.setString(3, title);
-	        insertReviewStatement.setString(4, body);
-	        insertReviewStatement.setDouble(5, rating);
+	        insertReviewStatement.setString(1, diningHall);
+	        insertReviewStatement.setString(2, username);
+	        insertReviewStatement.setString(3, password);
+	        insertReviewStatement.setString(4, title);
+	        insertReviewStatement.setString(5, body);
+	        insertReviewStatement.setDouble(6, rating);
 
 	        int affectedRows = insertReviewStatement.executeUpdate();
 
@@ -173,18 +174,21 @@ public class JDBC {
 	    }
 	}
 
+	
 	/**
 	//testing to insert a new review
 	public static void main(String[] args) {
 	    JDBC jdbcConnector = new JDBC();
 
+	    // Test review data
+	    String diningHall = "EVK";
 	    String username = "201Group";
 	    String password = "Group"; //
 	    String title = "Great!";
 	    String body = "Highly recommended this dining hall!";
 	    double rating = 5.0; 
 
-	    int result = jdbcConnector.insertReview(username, password, title, body, rating);
+	    int result = jdbcConnector.insertReview(diningHall, username, password, title, body, rating);
 
 	    if (result > 0) {
 	        System.out.println("Review inserted successfully");
@@ -195,7 +199,7 @@ public class JDBC {
 	**/
 	
 	//modify a review 
-	public int updateReview(int reviewID, String title, String body, Double rating) {
+	public int updateReview(int reviewID, String title, String body, Double rating, String diningHall) {
 	    try (Connection connection = getConnection()) {
 	        StringBuilder queryBuilder = new StringBuilder("UPDATE Reviews SET ");
 	        List<Object> params = new ArrayList<>();
@@ -211,6 +215,10 @@ public class JDBC {
 	        if (rating != null) {
 	            queryBuilder.append("rating = ?, ");
 	            params.add(rating);
+	        }
+	        if (diningHall != null) {
+	            queryBuilder.append("diningHall = ?, ");
+	            params.add(diningHall);
 	        }
 
 	        //Remove the trailing comma and space - ChatGPT 
@@ -241,17 +249,20 @@ public class JDBC {
 	    }
 	}
 
+
 	/**
 	public static void main(String[] args) {
 	    JDBC jdbcConnector = new JDBC();
 
-	    //have the review ID and updated review information
+	    // Assuming you have the review ID and updated review information
 	    int reviewIDToUpdate = 1; // Replace with the actual review ID to be updated
 	    String updatedTitle = "Hello";
-	    String updatedBody = null; //body unchanged
+	    String updatedBody = null; // To keep body unchanged
 	    Double updatedRating = 5.0; // Updated rating
-     
-	    int result = jdbcConnector.updateReview(reviewIDToUpdate, updatedTitle, updatedBody, updatedRating);
+	    String diningHall = "EVK";
+
+	    // Example usage:
+	    int result = jdbcConnector.updateReview(reviewIDToUpdate, updatedTitle, updatedBody, updatedRating, diningHall);
 
 	    if (result > 0) {
 	        System.out.println("Review updated successfully");
@@ -262,6 +273,61 @@ public class JDBC {
 	    }
 	}
 	**/
+	
+	//insert menu
+	//dininghall name, menu item, date
+	public int insertMenu(String diningHallName, String menuItem, Date date) {
+	    try (Connection connection = getConnection();
+	         PreparedStatement insertMenuStatement = connection.prepareStatement("INSERT INTO Menu (diningHallName, menuItem, date) VALUES (?, ?, ?)")) {
+
+	        insertMenuStatement.setString(1, diningHallName);
+	        insertMenuStatement.setString(2, menuItem);
+	        insertMenuStatement.setDate(3, new java.sql.Date(date.getTime())); // Assuming date is of type java.util.Date
+
+	        int affectedRows = insertMenuStatement.executeUpdate();
+
+	        if (affectedRows > 0) {
+	            System.out.println("Menu item inserted successfully");
+	            return 1;
+	        } else {
+	            System.out.println("Failed to insert menu item");
+	            return -1;
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return -2; // Error
+	    }
+	}
+	
+	/**
+	//test insert menu item
+	public static void main(String[] args) {
+        JDBC tester = new JDBC();
+
+        // Test menu data
+        String diningHallName = "Sample Dining Hall";
+        String menuItem = "Pasta";
+        Date date = new Date(); // Assuming today's date
+
+        // Test inserting a menu item
+        int result = tester.insertMenu(diningHallName, menuItem, date);
+
+        if (result > 0) {
+            System.out.println("Menu item inserted successfully");
+        } else if (result == -1) {
+            System.out.println("Failed to insert menu item");
+        } else {
+            System.out.println("Error occurred while inserting menu item");
+        }
+    }
+	
+	//dining hall reviews
+	public int averageDiningHallRating (String diningHallName, double rating) {
+		return 0;
+	}
+	**/
+	
+	
 
 	
 	
