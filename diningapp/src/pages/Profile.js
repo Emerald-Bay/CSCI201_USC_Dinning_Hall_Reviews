a42b5b04e92fddda71f97969a1f06cd4d4a1d308
Seeon Chang <zionchan@usc.edu>
2023-11-29 21:48:11 -0800

    profile

--- diningapp/src/pages/Profile.js
+++ diningapp/src/pages/Profile.js
@@ -1,31 +1,45 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Card, Image } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Profile = () => {
-    
-    const [user] = useState({ 
-        username: 'sampleUser', 
+    const [user, setUser] = useState({ 
+        username: '', 
         firstName: '',
         lastName: '',
-        timestsamp: '',
+        timestamp: '',
     });
-    
+
+    useEffect(() => {
+        const username = data.username;
+        fetch(`http://localhost:8080/UserProfileServlet?username=${encodeURIComponent(username)}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.username) {
+                    setUser(data);
+                } else {
+                    console.error('User not found');
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
+    }, []);
 
     return (
         <Container>
             <Row className='justify-content-md-center mt-3 mb-3'>
                 <Col className='text-center'>
-                    <h2>User Profile</h2>
+                    <h2>{user.firstName}'s Profile</h2>
                 </Col>
             </Row>
             <Row className='justify-content-md-center'>
                 <Col md={6}>
                     <Card>
                         <Card.Body>
-                            <Card.Title>{user.firstName}'s Profile</Card.Title>
+                            <Card.Title>{user.firstName} {user.lastName}</Card.Title>
                             <Card.Text>
                                 <p><b>Username:</b> {user.username}</p>
-
+                                <p><b>Join Date:</b> {user.timestamp}</p>
                             </Card.Text>
                         </Card.Body>
                     </Card>
