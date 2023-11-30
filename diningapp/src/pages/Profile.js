4f92a62fd9e459e348ec4ee8e8536df43e5af327
Seeon Chang <zionchan@usc.edu>
2023-11-29 21:13:33 -0800

    profile

--- diningapp/src/pages/Profile.js
+++ diningapp/src/pages/Profile.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Profile = () => {
+    // Sample user data, you would normally get this from a backend server
+    const [user, setUser] = useState({ username: 'sampleUser', password: 'password123' });
+
     return (
         <div>
-            <p>Profile Component</p>
+            <h1>Profile Component</h1>
+            <p><b>Username:</b> {user.username}</p>
+            <p><b>Password:</b> {user.password}</p>
         </div>
     );
 };
