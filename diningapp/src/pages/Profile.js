b6c3f1cdafe5b27636e0b7877db636007c7de246
Seeon Chang <zionchan@usc.edu>
2023-11-29 21:22:33 -0800

    profile

--- diningapp/src/pages/Profile.js
+++ diningapp/src/pages/Profile.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Profile = () => {
-    // Sample user data, you would normally get this from a backend server
+    
     const [user, setUser] = useState({ username: 'sampleUser', password: 'password123' });
 
     return (
-        <div>
-            <h1>Profile Component</h1>
-            <p><b>Username:</b> {user.username}</p>
-            <p><b>Password:</b> {user.password}</p>
-        </div>
+        <Container>
+            <Row className='justify-content-md-center mt-3 mb-3'>
+                <Col className='text-center'>
+                    <h2>User Profile</h2>
+                </Col>
+            </Row>
+            <Row className='justify-content-md-center'>
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>{user.username}'s Profile</Card.Title>
+                            <Card.Text>
+                                <p><b>Username:</b> {user.username}</p>
+                                {/* hidden */}
+                                {/* <p><b>Password:</b> {user.password}</p> */}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
