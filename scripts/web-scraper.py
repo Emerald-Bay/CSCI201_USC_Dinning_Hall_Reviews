890e6c57e3fe1c716e8e615cb6149fdcba000403
Egor Cherkashin <cherkash@usc.edu>
2023-11-30 19:31:21 -0800

    Connected to mySQL

--- scripts/web-scraper.py
+++ scripts/web-scraper.py
@@ -1,12 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import mysql.connector
+import datetime
 
 
 URL = "https://hospitality.usc.edu/residential-dining-menus/"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'}
 ALLERGENS = ["Dairy", "Eggs", "Fish", "Food Not Analyzed for Allergens", "Halal Ingredients", "Peanuts", "Pork", "Sesame", "Shellfish", "Soy", "Tree Nuts", "Vegan", "Vegetarian", "Wheat / Gluten"]
 
+
+def get_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='201Group',
+            user='root',
+            password='root'
+        )
+        return connection
+    except mysql.connector.Error as e:
+        print("Error while connecting to MySQL", e)
+        return None
+
+def insert_menu(dining_hall_name, menu_item, date):
+    connection = get_connection()
+    if connection is not None:
+        try:
+            cursor = connection.cursor()
+            query = "INSERT INTO Menu (diningHallName, menuItem, date) VALUES (%s, %s, %s)"
+            cursor.execute(query, (dining_hall_name, menu_item, date))
+            connection.commit()
+            return 1
+        except mysql.connector.Error as e:
+            print("Failed to insert menu item:", e)
+            return -2
+        finally:
+            connection.close()
+    else:
+        return -1
+
+def clear_table():
+    connection = get_connection()
+    if connection is not None:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("TRUNCATE TABLE Menu")
+            connection.commit()
+            print("Table cleared successfully")
+        except mysql.connector.Error as e:
+            print("Error clearing table:", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
 
 def format_menu_item(item):
     for allergen in ALLERGENS:
@@ -37,6 +84,17 @@ def get_menu_data():
 
     return menu_data
 
-
 menu_data = get_menu_data()
-print(json.dumps(menu_data, indent=4))
+clear_table()
+count = 0
+
+for dining_hall, meals in menu_data.items():
+    for meal_time, items in meals.items():
+        for item in items:
+            if(item == ""):
+                continue
+            current_date = datetime.date.today()
+            if(insert_menu(dining_hall, item, current_date) == 1):
+                count += 1
+
+print(count, "items inserted")
