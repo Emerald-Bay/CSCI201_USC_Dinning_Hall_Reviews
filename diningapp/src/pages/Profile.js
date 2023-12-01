2a883eb7176be0c7ed3bb661c9d8292ebba0c79b
Seeon Chang <zionchan@usc.edu>
2023-12-01 12:58:40 -0800

    profile.js

--- diningapp/src/pages/Profile.js
+++ diningapp/src/pages/Profile.js
@@ -2,51 +2,48 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
-
 const Profile = () => {
     const [loggedInUser, setLoggedInUser] = useState(null);
-    
+    const [user, setUser] = useState(null); // Initialize user as null
 
+    // Effect to retrieve stored user information
     useEffect(() => {
-        // Check if user data exists in local storage
         const storedUser = localStorage.getItem('loggedInUser');
         if (storedUser) {
-          // Parse the stored user data and set it in the state
-          setLoggedInUser(JSON.parse(storedUser));
+            setLoggedInUser(JSON.parse(storedUser));
         }
-      }, []); // The empty dependency array ensures this effect runs only once, similar to componentDidMount
+    }, []);
 
+    // Handler for logout
     const handleLogout = () => {
-        // Clear user data from state and local storage
         setLoggedInUser(null);
         localStorage.removeItem('loggedInUser');
     };
 
-    const [user, setUser] = useState({ 
-        username: 'sampleUsername', 
-        firstName: 'sampleFirstName',
-        lastName: 'sampleLastName',
-        timestamp: 'sampleTimestamp',
-    });
-
-
-    const username = 'the username of the logged-in user'; // Replace with actual username retrieval logic
-
+    // Effect to fetch user data from the servlet
     useEffect(() => {
-        fetch(`http://localhost:8080/UserProfileServlet?username=${encodeURIComponent(username)}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.username) {
-                    setUser(data);
-                } else {
-                    console.error('User not found');
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching user data:', error);
-            });
-    }, [username]); 
+        if (loggedInUser && loggedInUser.username) {
+            fetch(`http://localhost:8080/UserProfileServlet?username=${encodeURIComponent(loggedInUser.username)}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.username) {
+                        setUser(data);
+                    } else {
+                        console.error('User not found');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching user data:', error);
+                });
+        }
+    }, [loggedInUser]); // Depend on loggedInUser
 
+    // Loading state
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
+    // JSX for profile display
     return (
         <Container>
             <Row className='justify-content-md-center mt-3 mb-3'>
@@ -62,21 +59,20 @@ const Profile = () => {
                             <Card.Text>
                                 <p><b>Username:</b> {user.username}</p>
                                 <p><b>Join Date:</b> {new Date(user.timestamp).toLocaleDateString()}</p>
-                                {/* Assuming timestamp is in a format that can be converted to a Date */}
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
-            <Row className='justify-content-md-center mt-3 mb-3' >
+            <Row className='justify-content-md-center mt-3 mb-3'>
                 <Col md={12} className='text-center'>
-                {loggedInUser ? (
-                    <>
-                        <p>Welcome {loggedInUser.username}</p>
-                        <Button variant="primary" type="submit" onClick={handleLogout}>
+                    {loggedInUser ? (
+                        <>
+                            <p>Welcome {loggedInUser.username}</p>
+                            <Button variant="primary" type="submit" onClick={handleLogout}>
                                 Logout
                             </Button>
-                    </>
+                        </>
                     ) : null}
                 </Col>
             </Row>
